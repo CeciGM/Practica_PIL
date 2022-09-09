42a9b4eaaa08cb1df52311b1efc76fda5e070469
Cecilia Garcia <cecigarciam@gmail.com>
2022-09-08 21:16:47 -0300

    Modificacion de archivo con funciones print

--- Clase_1/entrada_salida_datos.py
+++ Clase_1/entrada_salida_datos.py
@@ -1,3 +1,8 @@
-#Entrada de datos
+#Funcion print
 print("Hola Mudno")
-Print("hi, he's new")
+print("hi, he's new")
+
+print("Hola Mundo\n") #el \n me deja un espacio debajo, hace un salto.
+print("Desde \nvisualstudio") #en el medio me separa las lineas, una debajo de otra.
+
+#Otras funciones del print
